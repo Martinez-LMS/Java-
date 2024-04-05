b46f6b89e4bddc4ed95ab2a02a3b75452177ed0d
Martinez-LMS <leo.ooosla@gmail.com>
2024-04-05 11:05:03 -0300

    com classe Pessoa

--- Pessoa.java
+++ Pessoa.java
@@ -13,15 +13,19 @@ public class Pessoa {
 
     }
 
+    public void setAltura(int altura) {
+        if (altura > 15 && altura < 250) {
+            this.altura = altura;
+        } else {
+            // throw Exception();
+        }
+
+    }
+
     public Pessoa(String nome, int altura, int peso) {
         this.nome = nome;
         this.altura = altura;
         this.peso = peso;
-    }
-
-    public void setNome(String nome) {
-
-        this.nome = nome;
     }
 
 }
