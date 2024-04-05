da413a2c968bf9d97aac8afbb1a4cea480143574
Martinez-LMS <leo.ooosla@gmail.com>
2024-04-05 10:44:20 -0300

    metodos GET E SET

---
+++ ProgramaCarro.java
@@ -0,0 +1,29 @@
+public class ProgramaCarro {
+    public static void main(String[] args) {
+
+        Carro uno = new Carro(144);
+
+        uno.setMarca("Fiat");
+        uno.setModelo("Uno Mille");
+        uno.acelerar();
+        uno.acelerar();
+        uno.acelerar();
+
+        uno.frear();
+        uno.frear();
+
+        System.out.println("marca: " + uno.getMarca());
+        System.out.println("marca: " + uno.getModelo());
+        System.out.println("Velocidade: " + uno.getVelocidade());
+
+        Carro bmw = new Carro(250);
+        bmw.setMarca("BMW");
+        bmw.setModelo("m3");
+
+        bmw.acelerar();
+        bmw.acelerar();
+        bmw.acelerar();
+        bmw.frear();
+
+    }
+}
